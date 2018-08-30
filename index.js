19f13733ba5703a57d16424a5c7bfb37dbeaa7ec
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-30 14:06:51 -0300

    ## Creating the Hapi.js Server

---
+++ index.js
@@ -0,0 +1,48 @@
+require('node-env-file')(`${__dirname}/.env`);
+
+const redis = require('redis');
+const createServer = require('./src/server.js');
+const {promisify} = require('util');
+
+const start = async () => {
+  const server = await createServer(
+    {
+      port: process.env.PORT,
+      host: process.env.HOST,
+    },
+    {
+      enableSSL: process.env.SSL === 'true',
+    }
+  );
+
+  const redisClient = redis.createClient(
+    {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }
+  );
+
+  redisClient.lpushAsync = promisify(redisClient.lpush).bind(redisClient);
+  redisClient.lrangeAsync = promisify(redisClient.lrange).bind(redisClient);
+  redisClient.llenAsync = promisify(redisClient.llen).bind(redisClient);
+  redisClient.lremAsync = promisify(redisClient.lrem).bind(redisClient);
+  redisClient.lsetAsync = promisify(redisClient.lset).bind(redisClient);
+
+  redisClient.on("error", function (err) {
+    console.error("Redis error.", err);
+  });
+
+  server.app.redis = redisClient;
+
+  await server.start();
+
+  console.log(`Server running at: ${server.info.uri}`);
+  console.log(`Server docs running at: ${server.info.uri}/docs`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+start();
